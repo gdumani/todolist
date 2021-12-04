7108be26cc447b24cfedf284136d1d7801d490cc
gdumani <gdumani@gmail.com>
2021-12-04 12:15:49 -0600

    add css and webpack server support

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
+   mode: 'development',
    entry: {
      index: './src/index.js',
    },
+   devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -15,4 +19,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  }
  };
