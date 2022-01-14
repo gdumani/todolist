a1eaaea0e3bc7e611ecf9863aeda6b65b13d7d50
gdumani <gdumani@gmail.com>
2022-01-14 06:57:07 -0600

    fix js linters reported by eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   devServer: {
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
     static: './dist',
   },
   plugins: [
@@ -14,17 +14,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
     ],
-  }
- };
+  },
+};
