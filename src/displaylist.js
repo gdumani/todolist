6e296d3dbb91fbe5dd6370ff78f6b193012c53e7
gdumani <gdumani@gmail.com>
2022-01-15 08:31:00 -0600

    implementa function to add new tasks

--- src/displaylist.js
+++ src/displaylist.js
@@ -1,15 +1,16 @@
 
 const displayList = (l) => {
-  console.log("lista en displayList",l)
+  let allLi = document.getElementById('list');
+  allLi.innerHTML = '';
   l.forEach((el) => {
     const liHtml = document.createElement('li');
     liHtml.innerHTML += `<input type="checkbox" class="checkTask" data-key=${el.index}>
-    <span>${el.task}</span>
+    <span>${el.description}</span>
     <button class = 'delTask'><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
     `;
     liHtml.className = 'listLine';
 
-    document.getElementById('list').appendChild(liHtml);
+    allLi.appendChild(liHtml);
   });
 }
 export default displayList;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import './style.css';
 import displayList from './displaylist.js';
 import getList from './getlist.js';
 import storeList from './storelist.js';
+import Task from './task.js';
 
 
-
-const testList = [
-  { task: 'Do dishes', completed: false, index: 1 },
-  { task: 'Do laundry', completed: false, index: 3 },
-  { task: 'Walk dog', completed: false, index: 2 },
-];
-storeList(testList);
+// const testList = [
+//   { task: 'Do dishes', completed: false, index: 1 },
+//   { task: 'Do laundry', completed: false, index: 3 },
+//   { task: 'Walk dog', completed: false, index: 2 },
+// ];
+// storeList(testList);
 const list = getList;
-console.log(list);
+const newT = document.querySelector('#newToDo');
+newT.addEventListener('keypress',(ev) => {
+  if (ev.key === 'Enter' && ev.value !== '') {
+    const task = new Task(newT.value, list.length)
+    list.push(task);
+    newT.value = '';
+    storeList(list);
+    diplayList(list);
+  }
+});
 displayList(list);
