736eb8aeebeddd9ed5edbfbf5b1922bf41e2289c
gdumani <gdumani@gmail.com>
2022-01-15 16:52:34 -0600

    change displayList rendering and added status change

--- src/displaylist.js
+++ src/displaylist.js
@@ -1,16 +1,32 @@
+import storeList from "./storelist";
 
 const displayList = (l) => {
-  let allLi = document.getElementById('list');
+  const allLi = document.getElementById('list');
   allLi.innerHTML = '';
   l.forEach((el) => {
     const liHtml = document.createElement('li');
-    liHtml.innerHTML += `<input type="checkbox" class="checkTask" data-key=${el.index}>
-    <span>${el.description}</span>
-    <button class = 'delTask'><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
-    `;
     liHtml.className = 'listLine';
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkBox';
+    checkBox.className = 'checkTask';
+    checkBox.value = el.completed;
+    checkBox.addEventListener('change', () => {
+      el.completed = !el.completed;
+      storeList(l);
+    });
+    const description = document.createElement('input');
+    description.type = 'text';
+    description.className = 'editClass';
+    description.value = el.description;
+    const delButton = document.createElement('button');
+    delButton.className = 'delTask';
+    delButton.innerHTML = '<i class="fa fa-ellipsis-v" aria-hidden="true">';
 
+    liHtml.appendChild(checkBox);
+    liHtml.appendChild(description);
+    liHtml.appendChild(delButton);
     allLi.appendChild(liHtml);
   });
-}
+};
+
 export default displayList;
